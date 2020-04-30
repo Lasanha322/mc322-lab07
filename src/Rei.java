6b473e92d70e6e7486d55fb55648781229ba938e
b194966 <62315950+b194966@users.noreply.github.com>
2020-04-30 20:51:23 -0300

    Add files via upload

--- src/Rei.java
+++ src/Rei.java
@@ -11,4 +11,19 @@ public class Rei extends Campo {
 		else
 			System.out.print("k ");
 	}
+	//o rei pode se mover apenas uma casa em todas as direções
+	boolean verificaMovimento(Campo[][] campo, int x, int y) {
+		boolean muv=false;
+		if(x==this.x+1 || x==this.x-1 ) {
+			if(y==this.y-1 || y==this.y || y==this.y+1 ) {
+				muv=true;
+			}
+		}
+		if(x==this.x) {
+			if(y==this.y-1 || y==this.y+1 ) {
+				muv=true;
+			}
+		}
+		return muv;
+	}
 }
