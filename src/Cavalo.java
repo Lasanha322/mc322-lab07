932ec3340d16f2f97d5da13f677182ecf2f046c7
b194966 <62315950+b194966@users.noreply.github.com>
2020-04-30 20:41:48 -0300

    Add files via upload

--- src/Cavalo.java
+++ src/Cavalo.java
@@ -11,4 +11,21 @@ public class Cavalo extends Campo {
 		else
 			System.out.print("h ");
 	}
+	//compara as posições possiveis para o cavalo com a posição que quer avançar
+	boolean verificaMovimento(Campo[][] campo, int x, int y) {
+		boolean muv=false;
+		if((campo[x][y].corPreta != corPreta && campo[x][y].getPeca()== true)|| campo[x][y].getPeca()== false) {
+			if(x == this.x-2 || x == this.x+2 ) {
+				if(y==this.y-1 || y==this.y+1) {
+					muv=true;
+				}
+			}
+			if(x == this.x-1 || x == this.x+1) {
+				if(y==this.y+2 || y==this.y-2) {
+					muv=true;
+				}
+			}
+		}
+		return muv;
+	}
 }
