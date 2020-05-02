09fe87f4564ded74e731e65b00edb7d51697c6fe
b194966 <62315950+b194966@users.noreply.github.com>
2020-05-02 15:37:07 -0300

    Add files via upload

--- src/Bispo.java
+++ src/Bispo.java
@@ -1,8 +1,9 @@
 public class Bispo extends Campo {	
-	
+	private int fatorX, fatorY, somadorX, somadorY;
 	Bispo (int x, int y, boolean corPreta) {
 		super(x, y);
 		this.corPreta = corPreta;
+		peca=true;
 	}
 	
 	public void mostrarCampo() {
@@ -10,5 +11,45 @@ public class Bispo extends Campo {
 			System.out.print("B ");
 		else
 			System.out.print("b ");
+	}
+	public boolean verificaMovimento(Campo[][] campo, int x, int y) {
+		boolean muv=false;
+		if(x!=this.x || y!=this.y) {
+			if (x > this.x) {
+				fatorX = 1;
+			}
+			if(x < this.x)
+				fatorX = -1;
+	
+			if (y > this.y) {
+				fatorY = 1;
+			} 
+			if(y < this.y) {
+				fatorY = -1;
+			}
+	
+			somadorX = fatorX;
+			somadorY = fatorY;
+			muv = movimento(campo, x, y);
+		}
+		return muv;
+	}
+	public boolean movimento(Campo[][] campo, int x, int y) {
+		boolean muv=false;
+		if (x != this.x + fatorX && y != this.y + fatorY) {
+			if (campo[this.x + fatorX][this.y + fatorY].getPeca() == false) {
+				fatorX = fatorX + somadorX;
+				fatorY = fatorY + somadorY;
+				muv = movimento(campo, x, y);
+			}
+		}
+		if (x == this.x + fatorX && y == this.y + fatorY) {
+			if (campo[this.x + fatorX][this.y + fatorY].getPeca() == false
+					|| (campo[this.x + fatorX][this.y + fatorY].getPeca() == true
+							&& campo[this.x + fatorX][this.y + fatorY].getCor() != corPreta)) {
+				muv = true;
+			}
+		}
+		return muv;
 	}
 }
