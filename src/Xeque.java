4f83dba9f7a02c773766ffc6ef0eede9be531888
b194966 <62315950+b194966@users.noreply.github.com>
2020-05-05 00:42:22 -0300

    Add files via upload
    ja compilei, falta pensar como aplicar ele no jogo

--- src/Xeque.java
+++ src/Xeque.java
@@ -1,9 +1,14 @@
 public class Xeque {
-	private int x, y;
+	protected int x, y;
+
+	Xeque(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
 	public boolean verificaXeque(Campo[][] campo, boolean cor) {
 		boolean muv = false;
-		int x, y;
+		// procura posição do rei
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				if (campo[i][j].getClass() == Rei.class && campo[i][j].getCor() == cor) {
@@ -14,31 +19,32 @@ public class Xeque {
 		}
 		// verificando possibilidade de adversarios
 		// verifica se tem bispo
-		muv = bispo(campo, cor);
+		muv = bishop(campo, cor);
 		// verifica se tem cavalo
 		if (muv == false) {
-			muv = cavalo(campo, cor);
+			muv = horse(campo, cor);
 		}
 		// verifica se tem peao
 		if (muv == false) {
-			muv = peao(campo, cor);
+			muv = paw(campo, cor);
 		}
 		// verifica se tem torre
 		if (muv == false) {
-			muv = torre(campo, cor);
+			muv = tower(campo, cor);
 		}
 		// verifica se tem rei
 		if (muv == false) {
-			muv = rei(campo, cor);
+			muv = king(campo, cor);
 		}
 		// verifica se tem rainha
 		if (muv == false) {
-			muv = rainha(campo, cor);
+			muv = queen(campo, cor);
 		}
 		return muv;
 	}
+
 //procura nas direções possiveis de cada peça
-	public boolean rainha(Campo[][] campo, boolean cor) {
+	public boolean queen(Campo[][] campo, boolean cor) {
 		boolean muv = false;
 		// leste
 		muv = direcoes(campo, cor, 0, 1, new Rainha(0, 0, false));
@@ -74,22 +80,26 @@ public class Xeque {
 		return muv;
 	}
 
-	public boolean rei(Campo[][] campo, boolean cor) {
+	public boolean king(Campo[][] campo, boolean cor) {
 		boolean muv = false;
-		if (campo[x + 1][y + 1].getClass() == Rei.class && campo[x + 1][y + 1].getCor() != cor
-				|| campo[x + 1][y - 1].getClass() == Rei.class && campo[x + 1][y - 1].getCor() != cor
-				|| campo[x - 1][y + 1].getClass() == Rei.class && campo[x - 1][y + 1].getCor() != cor
-				|| campo[x - 1][y - 1].getClass() == Rei.class && campo[x - 1][y - 1].getCor() != cor
-				|| campo[x][y - 1].getClass() == Rei.class && campo[x][y - 1].getCor() != cor
-				|| campo[x][y + 1].getClass() == Rei.class && campo[x][y = 1].getCor() != cor
-				|| campo[x - 1][y].getClass() == Rei.class && campo[x - 1][y].getCor() != cor
-				|| campo[x + 1][y].getClass() == Rei.class && campo[x + 1][y].getCor() != cor) {
+		if (this.x + 1 < 8 && this.y + 1 < 8 && campo[x + 1][y + 1].getClass() == Rei.class
+				&& campo[x + 1][y + 1].getCor() != cor
+				|| this.x + 1 < 8 && this.y - 1 > -1 && campo[x + 1][y - 1].getClass() == Rei.class
+						&& campo[x + 1][y - 1].getCor() != cor
+				|| this.x - 1 > -1 && this.y + 1 < 8 && campo[x - 1][y + 1].getClass() == Rei.class
+						&& campo[x - 1][y + 1].getCor() != cor
+				|| this.x - 1 > -1 && this.y - 1 > -1 && campo[x - 1][y - 1].getClass() == Rei.class
+						&& campo[x - 1][y - 1].getCor() != cor
+				|| this.y - 1 > -1 && campo[x][y - 1].getClass() == Rei.class && campo[x][y - 1].getCor() != cor
+				|| this.y + 1 < 8 && campo[x][y + 1].getClass() == Rei.class && campo[x][y = 1].getCor() != cor
+				|| this.x - 1 > -1 && campo[x - 1][y].getClass() == Rei.class && campo[x - 1][y].getCor() != cor
+				|| this.x + 1 < 8 && campo[x + 1][y].getClass() == Rei.class && campo[x + 1][y].getCor() != cor) {
 			muv = true;
 		}
 		return muv;
 	}
 
-	public boolean torre(Campo[][] campo, boolean cor) {
+	public boolean tower(Campo[][] campo, boolean cor) {
 		boolean muv = false;
 		// oeste
 		muv = direcoes(campo, cor, 0, -1, new Torre(0, 0, false));
@@ -108,40 +118,47 @@ public class Xeque {
 		return muv;
 	}
 
-	public boolean peao(Campo[][] campo, boolean cor) {
+	public boolean paw(Campo[][] campo, boolean cor) {
 		boolean muv = false;
-		if (campo[x + 1][y + 1].getClass() == Peao.class && campo[x + 1][y + 1].getCor() != cor
-				|| campo[x + 1][y - 1].getClass() == Peao.class && campo[x + 1][y - 1].getCor() != cor
-				|| campo[x - 1][y + 1].getClass() == Peao.class && campo[x - 1][y + 1].getCor() != cor
-				|| campo[x - 1][y - 1].getClass() == Peao.class && campo[x - 1][y - 1].getCor() != cor) {
+		if (this.x + 1 < 8 && this.y + 1 < 8 && campo[x + 1][y + 1].getClass() == Peao.class
+				&& campo[x + 1][y + 1].getCor() != cor
+				|| this.x + 1 < 8 && this.y - 1 > -1 && campo[x + 1][y - 1].getClass() == Peao.class
+						&& campo[x + 1][y - 1].getCor() != cor
+				|| this.x - 1 > -1 && this.y + 1 < 8 && campo[x - 1][y + 1].getClass() == Peao.class
+						&& campo[x - 1][y + 1].getCor() != cor
+				|| this.x - 1 > -1 && this.y - 1 > -1 && campo[x - 1][y - 1].getClass() == Peao.class
+						&& campo[x - 1][y - 1].getCor() != cor) {
 			muv = true;
 		}
 		return muv;
 	}
 
-	public boolean cavalo(Campo[][] campo, boolean cor) {
+	public boolean horse(Campo[][] campo, boolean cor) {
 		boolean muv = false;
-		if (campo[this.x - 2][this.y - 1].getClass() == Cavalo.class && campo[this.x - 2][this.y - 1].getCor() != cor
-				|| campo[this.x - 2][this.y + 1].getClass() == Cavalo.class
-						&& campo[this.x - 2][this.y + 1].getCor() != cor
-				|| campo[this.x + 2][this.y - 1].getClass() == Cavalo.class
-						&& campo[this.x + 2][this.y - 1].getCor() != cor
-				|| campo[this.x + 2][this.y + 1].getClass() == Cavalo.class
+		if ((this.x - 2 > -1 && this.y - 1 > -1 && campo[this.x - 2][this.y - 1].getClass() == Cavalo.class
+				&& campo[this.x - 2][this.y - 1].getCor() != cor)
+				|| this.x - 2 > -1 && this.y + 1 < 8
+						&& (campo[this.x - 2][this.y + 1].getClass() == Cavalo.class
+								&& campo[this.x - 2][this.y + 1].getCor() != cor)
+				|| this.x + 2 < 8 && this.y - 1 > -1
+						&& (campo[this.x + 2][this.y - 1].getClass() == Cavalo.class
+								&& campo[this.x + 2][this.y - 1].getCor() != cor)
+				|| this.x + 2 < 8 && this.y + 1 < 8 && campo[this.x + 2][this.y + 1].getClass() == Cavalo.class
 						&& campo[this.x + 2][this.y + 1].getCor() != cor
-				|| campo[this.x - 1][this.y - 2].getClass() == Cavalo.class
+				|| this.x - 1 > -1 && this.y - 2 > -1 && campo[this.x - 1][this.y - 2].getClass() == Cavalo.class
 						&& campo[this.x - 1][this.y - 2].getCor() != cor
-				|| campo[this.x - 1][this.y + 2].getClass() == Cavalo.class
+				|| this.x - 1 > -1 && this.y + 2 > 8 && campo[this.x - 1][this.y + 2].getClass() == Cavalo.class
 						&& campo[this.x - 1][this.y + 2].getCor() != cor
-				|| campo[this.x + 1][this.y - 2].getClass() == Cavalo.class
+				|| this.x + 1 < 8 && this.y - 2 > -1 && campo[this.x + 1][this.y - 2].getClass() == Cavalo.class
 						&& campo[this.x + 1][this.y - 2].getCor() != cor
-				|| campo[this.x + 1][this.y + 2].getClass() == Cavalo.class
+				|| this.x + 1 < 8 && this.y + 2 < 8 && campo[this.x + 1][this.y + 2].getClass() == Cavalo.class
 						&& campo[this.x + 1][this.y + 2].getCor() != cor) {
 			muv = true;
 		}
 		return muv;
 	}
 
-	public boolean bispo(Campo[][] campo, boolean cor) {
+	public boolean bishop(Campo[][] campo, boolean cor) {
 		boolean muv = false;
 		// nordeste
 		muv = direcoes(campo, cor, -1, 1, new Bispo(0, 0, false));
@@ -159,39 +176,25 @@ public class Xeque {
 		}
 		return muv;
 	}
+
 //anda pela direção indicada
-	public boolean  direcoes(Campo[][] campo, boolean cor,int i,int j, Campo p) {
+	public boolean direcoes(Campo[][] campo, boolean cor, int i, int j, Campo p) {
 		boolean muv = false;
-		int m1=0,M=0,s1=0,S=0;
-		if(i<0) {
-			m1=7;
-			s1=-1;
-		}
-		else if(i==0) {
-			s1=0;
-		}else {
-			m1=0;
-			s1=1;
-		}
-		
-		if(j<0) {
-			M=7;
-			S=-1;
-		}
-		else if(j==0) {
-			S=0;
-		}else {
-			M=7;
-			S=1;
-		}
-		
+		int s1, s2;
+		s1 = i;
+		s2 = j;
+
 		do {
-			if (campo[x+i][y + j].getClass() == p.getClass() && campo[x + i][y + j].getCor() != cor) {
-				muv = true;
+			if (x + s1 >= 0 && x + s1 <= 7 && y + s2 >= 0 && y + s2 <= 7) {
+				if ((campo[x + s1][y + s2].getClass() == p.getClass()) && campo[x + s1][y + s2].getCor() != cor) {
+					muv = true;
+				}
 			}
-			i+=s1;
-			j+=S;
-		} while (x + i <=m1 && i!=0 || y + j <=M && j!=0|| campo[x + i][y + j].getPeca() == true || muv==true);
+			s1 = i + s1;
+			s2 = j + s2;
+
+		} while ((x + s1 > -1 && x + s1 < 8 && y + s2 > -1 && y + s2 < 8) && muv == false
+				&& campo[x + s1 - i][y + s2 - j].getPeca() == false);
 		return muv;
-		}
+	}
 }
